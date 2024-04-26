5a7e4906c8387094d56a5d37df6e352ce3cdc35a
Wong Chun Hong <wchjohn2@gmail.com>
2024-04-27 00:23:13 +0800

    update the preview

--- src/figure/preview/Preview.jsx
+++ src/figure/preview/Preview.jsx
@@ -6,6 +6,7 @@ import OptionBar from '../optionBar/OptionBar'
 import { Rnd } from "react-rnd";
 import { ReactTinyLink } from 'react-tiny-link'
 import { onClickOutsideFigure, onSelectFigure, onChangeSizeAndPosition, figureIsEqual } from '../utils.mjs'
+import axios from 'axios';
 
 
 const Preview = memo(({x, y, backgroundColor, width, height, id, url, zIndex, scale, sendWebSocketMessage}) => {
@@ -13,37 +14,60 @@ const Preview = memo(({x, y, backgroundColor, width, height, id, url, zIndex, sc
   // since when the component move or resize, it will rerender once, then the websocket rerender twice
   console.log(`Preview - ${id}`);
 
+  useEffect(() => {
+    const getInfo = async () => {
+      const response = await axios.get(`${Config.url}/preview`, { params: { url: url} });
+      setPreviewData(response.data);
+    }
+    getInfo();
+  }, [url])
+
   // run when change in value of x, y, width, height
   useEffect(() => {
     console.log(`Resize Preview - ${id}`);
     setSizeAndPosition({x: x, y: y, width: width, height: height})
   }, [x, y, width, height]);
 
+  /*
   useEffect(() => {
     const object = document.getElementById(`${id}`);
     const previewContent = object.getElementsByClassName('react_tinylink_card_content_wrapper');
     previewContent[0].style.backgroundColor = `${backgroundColor}`;
     previewContent[0].classList.add(`${id}-noDrag`);
   });
+  */
 
   const [sizeAndPosition, setSizeAndPosition] = useState({x: x, y: y, width: width, height: height});
+  const [previewData, setPreviewData] = useState();
   const wrapperRef = useRef(null);
   onClickOutsideFigure(wrapperRef, id, null, null);  
 
-  return (
-    <Rnd enableResizing={Config.objectResizingDirection} size={{ width: sizeAndPosition.width, height: sizeAndPosition.height }} position={{ x: sizeAndPosition.x, y: sizeAndPosition.y }} 
-      bounds="#interface" cancel={`.${id}-noDrag`} style={{zIndex: `${zIndex}`}}
-      minWidth={Config.figureMinWidth} minHeight={Config.figureMinHeight} maxWidth={Config.figureMaxWidth} maxHeight={Config.figureMaxHeight}  
-      onMouseDown={(event) => onSelectFigure(event, id, null, null)} draggable={false} scale={scale}
-      onResizeStop={(e, direction, ref, delta, position) => onChangeSizeAndPosition(sizeAndPosition, { x: position.x, y: position.y, width: ref.style.width.replace("px", ""), height: ref.style.height.replace("px", "") }, setSizeAndPosition, id, sendWebSocketMessage)}
-      onDragStop={(e, data) => onChangeSizeAndPosition(sizeAndPosition, { x: data.x, y: data.y, width: sizeAndPosition.width, height: sizeAndPosition.height}, setSizeAndPosition, id, sendWebSocketMessage)}>
+  if (previewData == null) {
+    return (<div></div>)
+  }
+  else {
+    return (
+      <Rnd enableResizing={Config.objectResizingDirection} size={{ width: sizeAndPosition.width, height: sizeAndPosition.height }} position={{ x: sizeAndPosition.x, y: sizeAndPosition.y }} 
+        bounds="#interface" cancel={`.${id}-noDrag`} style={{zIndex: `${zIndex}`}}
+        minWidth={Config.figureMinWidth} minHeight={Config.figureMinHeight} maxWidth={Config.figureMaxWidth} maxHeight={Config.figureMaxHeight}  
+        onMouseDown={(event) => onSelectFigure(event, id, null, null)} draggable={false} scale={scale}
+        onResizeStop={(e, direction, ref, delta, position) => onChangeSizeAndPosition(sizeAndPosition, { x: position.x, y: position.y, width: ref.style.width.replace("px", ""), height: ref.style.height.replace("px", "") }, setSizeAndPosition, id, sendWebSocketMessage)}
+        onDragStop={(e, data) => onChangeSizeAndPosition(sizeAndPosition, { x: data.x, y: data.y, width: sizeAndPosition.width, height: sizeAndPosition.height}, setSizeAndPosition, id, sendWebSocketMessage)}>
+  
+        <div id={id} ref={wrapperRef} className='preview-container' style={{backgroundColor: `${backgroundColor}`}}>
+          <OptionBar id={id} backgroundColor={backgroundColor} sendWebSocketMessage={sendWebSocketMessage} />
+          <img src={previewData.image} className='preview-media'draggable={false} />
+          <a className={`${id}-noDrag preview-content`} target="_blank" href={previewData.url}>
+            <p className='preview-text preview-title'>{previewData.title}</p>
+            <p className='preview-text'>{previewData.description}</p>
+            <p className='preview-text'>{previewData.url}</p>
+          </a>
+      </div>   
+      </Rnd>
+    )
+  }
 
-      <div id={id} ref={wrapperRef} style={{height: "100%", width: "100%", overflow: "hidden"}} >
-        <OptionBar id={id} backgroundColor={backgroundColor} sendWebSocketMessage={sendWebSocketMessage} />
-        <ReactTinyLink cardSize="large" width={sizeAndPosition.width} height={sizeAndPosition.height} showGraphic={true} url={url} proxyUrl={`${Config.url}/proxy`} />
-      </div>    
-    </Rnd>
-  )
+  
 }, figureIsEqual);
 
 
